fa72b2ba1104c547076cb990e04dde9baa834701
Girafepothame <45842897+Girafepothame@users.noreply.github.com>
2022-01-12 22:25:47 +0100

    Update lecture_elf.c

--- elf_linker-1.0/lecture_elf.c
+++ elf_linker-1.0/lecture_elf.c
@@ -631,7 +631,7 @@ void afficheContenuNumero(int valeur, FILE *f){
 		
 			if( i%16==0){
 				printf("\n");
-				if(bswap_32(sct[valeur].sect.sh_size) == 16){
+				if(sct[valeur].sect.sh_size == 16){
 					printf("0x%08x", sct[valeur].sect.sh_addr);
 				}else{
 					printf("0x%08x", taille);
@@ -678,6 +678,7 @@ void afficheContenuString(char *valeur, FILE *f){
 	printf("index section : %d", index);
 	afficheContenuNumero(index, f);
 }
+
 
 
 
@@ -755,93 +756,4 @@ void print_symbole() {
 	printf("%s",sym[j].nom);
 	printf("\n");
 	}
-}
-
-int main(int argc , char **argv)
-{
-    char *entree = NULL; // Nom du fichier en entrée.
-	int aff_all = 0; 	 // Tout afficher
-    int aff_hd = 0;      // Est-ce qu'on affiche le header du fichier ? (-h)
-    int aff_sc = 0;      // Est-ce qu'on affiche les headers des sections ? (-S)
-	int aff_sb = 0;      // Est-ce qu'on affiche la table des symboles ? (-s)
-	int aff_rel = 0;     // Est-ce qu'on affiche la table de réalocation ? (-r)
-    for (int i=1; i<argc; i++) {
-        if (!strcmp(argv[i], "-h")) {
-            aff_hd = 1;
-        } else {
-            if (!strcmp(argv[i], "-S")) {
-                aff_sc = 1;
-            } else {
-				if (!strcmp(argv[i], "-s")) {
-					aff_sb = 1;
-				}else if (!strcmp(argv[i], "-r")) {
-					aff_rel = 1;
-				}else if (entree==NULL) {
-                    // Le premier argument qui n'est pas une option est le nom du fichier d'entrée.
-                    entree = argv[i];
-                }
-            }
-        }
-    }
-	if(argc == 2) {
-		aff_all = 1;
-	}
-    if (entree==NULL) {
-        printf("Aucun nom de fichier donné en argument.\n");
-        return 2;
-    }
-
-    FILE *f = fopen(entree,"r");
-    lectureHead(f);
-    fclose(f);
-    
-    
-    sct = malloc(sizeof(section_n) * header.e_shnum);
-    
-    f = fopen(entree,"r");
-    lectureSection(f);
-    fclose(f);
-
-
-	f = fopen(entree,"r");
-    lectureSymbol(f);
-    fclose(f);
-
-	f = fopen(entree,"r");
-	lectureReloc(f);
-	fclose(f);
-
-	
-
-    if (aff_hd){
-        print_header();
-	}
-	
-    if (aff_sc){
-        print_section();
-	}
-	
-    if (aff_sb) {
-        print_symbole();
-	}
-
-	if (aff_rel) {
-    	printReloc();
-	}
-
-	if (aff_all) {
-        print_header();
-        print_section();
-        print_symbole();
-	}
-
-	if (argv[2] != NULL && isNumber(argv[2])) { // à voir pour automatiser si argv[2] est int ou char*
-		f = fopen(entree,"r");
-		int num = atoi(argv[2]);
-		//char *num = argv[2];
-		afficheContenuNumero(num, f);
-    	fclose(f);
-	}
-
-    return 0;
 }
